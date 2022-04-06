a147f7d46172b43d9c97759c90ca63637235ec54
Yusuke Ando <yandod@gmail.com>
2022-04-06 16:35:57 +0900

    convert result to record

--- src/update.ts
+++ src/update.ts
@@ -13,7 +13,20 @@ const main = async () => {
     const cTable = cheerio.load(table);
     cheerioTableparser(cTable);
     const data = cTable('table').parsetable();
-    console.log(data);
+
+    const new_record = {
+        'date': new Date().toISOString().split('T')[0],
+        'confirmed_cases': data[1][0].match(/\d+/)[0],
+        'inpatient_care': data[1][1].match(/\d+/)[0],
+        'hotel_care': data[1][2].match(/\d+/)[0],
+        'home_care': data[1][3].match(/\d+/)[0],
+        'cured': data[1][4].match(/\d+/)[0],
+        'deaths': data[1][5].match(/\d+/)[0],
+    };
+
+    console.log(new_record);
+
+    
 };
 
 main();
